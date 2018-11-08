3533f697aec8c6cac3b9a4a46716eb2f13fef2d8
24seconds <24crazyoung@gmail.com>
2018-11-08 19:24:56 +0000

    Change priority color without differentiating id

--- public/javascripts/list_manage.js
+++ public/javascripts/list_manage.js
@@ -1,45 +1,53 @@
 var list_count = 10;
 var list_index = 0;
 
-$(function(){
-    $("#modal_button").click(function(){
-        var memoTitle = $("#memoTitle");
-        var date = $("#memoDate");
-        var memoContent = $("#memoContent");
-        var priority_buton = $("#modal-priority-button");
-        var colorArray = ["rgb(244, 67, 54)","rgb(255, 193, 7)","rgb(76, 175, 80)", ""];
-        var colorArray2 = [ "#f44336", "#ffc107", "#4CAF50", ""]; // w3-red, w3-amber, w3-green
-        var color = colorArray.indexOf(priority_buton.css("background-color"));
-        console.log(memoTitle.val());
-        console.log(date.val());
-        console.log(memoContent.val());
-        console.log(priority_buton.css("background-color"));
-        console.log(color);
-        $("#id01").css("display","none");
+$(function () {
+    // $("#modal_button").click(setTimeout(function(){
+    //     alert("test");
+    // }, 0));
+    $("#modal_button").click(function () {
+        setTimeout(function () {
+            var memoTitle = $("#memoTitle");
+            var date = $("#memoDate");
+            var memoContent = $("#memoContent");
+            var priority_buton = $("#modal-priority-button");
+            var colorArray = ["rgb(244, 67, 54)", "rgb(255, 193, 7)", "rgb(76, 175, 80)", ""];
+            var colorArray2 = ["#f44336", "#ffc107", "#4CAF50", ""]; // w3-red, w3-amber, w3-green
+            var color = colorArray.indexOf(priority_buton.css("background-color"));
+            console.log(memoTitle.val());
+            console.log(date.val());
+            console.log(memoContent.val());
+            console.log(priority_buton.css("background-color"));
+            console.log(color);
+            $("#id01").css("display", "none");
 
-        if(color === -1){ color = 3}
-        
-        // alert("hi there~");
-        var html = '<li><div class="notice-box" , id="'+"test"+list_count+'"><div class="priority-layout"><div class="priority" id="priority-color'+color+'"></div></div>'
-        html += '<div class="memo-layout"><div class="metadata-layout"><div class="title-layout"><div>'+memoTitle.val()+'</div></div>'
-        html += '<span id="close">&times;</span><!-- &times; == x button --></div>'
-        html += '<div class="content-layout"><p>'+memoContent.val()+'</p></div></div></div></li>'
+            if (color === -1) { color = 3 }
 
-        $("#list").append(html);
-        memoTitle.val('');
-        date.val('');
-        memoContent.val('');
-        priority_buton.css("background-color", "");
-    });
+            // alert("hi there~");
+            var html = '<li><div class="notice-box" , id="' + "test" + list_count + '"><div class="priority-layout"><div class="priority" id="priority-color' + '' + '"></div></div>'
+            html += '<div class="memo-layout"><div class="metadata-layout"><div class="title-layout"><div>' + memoTitle.val() + '</div></div>'
+            html += '<span id="close">&times;</span><!-- &times; == x button --></div>'
+            html += '<div class="content-layout"><p>' + memoContent.val() + '</p></div></div></div></li>'
 
-    $("#list").on("click", "#close", function(){
+            $("#list").append(html);
+            $("#test"+list_count).children('.priority-layout').children('.priority').css("background-color", colorArray[color]);
+            memoTitle.val('');
+            date.val('');
+            memoContent.val('');
+            priority_buton.css("background-color", "");
+            list_count +=1;
+        }, 0);
+    })
+
+
+    $("#list").on("click", "#close", function () {
         $(this).parent().parent().parent().parent().remove();
     })
 
-    $("#list").on("click",".memo-layout", function(){
+    $("#list").on("click", ".memo-layout", function () {
         $(this).children('.metadata-layout').children('.title-layout').children('div').wrap("<strike>");
         $(this).children('.content-layout').children('p').wrap("<strike>");
-        $(this).children('.metadata-layout').children('.title-layout').css("background-color","#6e6e64");
+        $(this).children('.metadata-layout').children('.title-layout').css("background-color", "#6e6e64");
     })
 
     // $("#list").on("click", ".btnDel", function(){
